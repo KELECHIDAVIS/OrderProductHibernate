b3af27fb7190136919c2d97ea5151d5474cad3cb
KELECHIDAVIS <kboss16058@gmail.com>
2025-07-02 17:11:40 -0700

    commit

--- src/main/java/org/example/MainCreateOrder.java
+++ src/main/java/org/example/MainCreateOrder.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class MainCreateOrder {
+
+    public static void main(String[] args){
+            SessionFactory factory = new Configuration().
+                configure("src/main/hibernate.cfg.xml").
+                addAnnotatedClass(Order.class).
+                buildSessionFactory();
+
+
+    }
 }
